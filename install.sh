ec21f43c1ab8f3473f1d3e6f3c622d450c2b03c0
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-07-05 23:33:20 +0700

    Set zsh default (#14)

--- install.sh
+++ install.sh
@@ -34,10 +34,10 @@ function install_zsh {
     echo "Installing zsh and oh-my-zsh..."
 
     sudo apt-get install -y zsh
+    sudo chsh -s /usr/bin/zsh
+    exec zsh
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-    sudo chsh -s /usr/bin/zsh
 
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
     
