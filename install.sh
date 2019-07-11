24ffccb2cb5d87c60e3274dd01f94a0738d6bc73
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-07-11 17:32:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,6 @@ function install_zsh {
     sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
     sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
    
-    exec zsh
 }
 
 
