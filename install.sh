ae63de92209ef9607c3b8312e956f7352a486aff
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-07-17 00:14:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,14 +33,11 @@ function install_zsh {
 
     echo "Installing zsh and oh-my-zsh..."
 
-    sudo apt install zsh
-    
-    # Set zsh default shell
-    sudo chsh -s $(which zsh)
-    
-    exec zsh
+    sudo apt-get install -y zsh
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    sudo chsh -s /usr/bin/zsh
 
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
     
