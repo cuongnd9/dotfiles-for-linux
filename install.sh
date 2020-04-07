081959abb524718a4ed800345f604740b44e72ca
Cuong Duy Nguyen <cuongw.me@gmail.com>
2020-04-08 02:32:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ function initialize {
     sudo apt update
     sudo apt install -y curl
     sudo apt install -y git
-    git config --global user.email '123cuong@gmail.com'
+    git config --global user.email '103cuong@gmail.com'
     git config --global user.name 'Cuong Duy Nguyen'
 
 }
